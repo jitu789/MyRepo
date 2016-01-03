0e5bba6412244a393246dd2777c10a00817d4674
jitu789 <jitu789@gmail.com>
2016-01-03 23:21:03 +0530

    welcome

---
+++ M1.java
@@ -0,0 +1,28 @@
+class Mul
+{
+int a,b,c;
+void acc(int m,int n)
+{
+a=m;
+b=n;}
+void mult()
+{
+c=a*b;
+System.out.println(c);
+}
+void disp()
+{
+System.out.println(a+" "+b);
+}}
+class M1
+{
+public static void main(String s[])
+{
+int x=Integer.parseInt(s[0]);
+int y=Integer.parseInt(s[1]);
+Mul m=new Mul();
+m.acc(x,y);
+m.mult();
+m.disp();
+}
+}
